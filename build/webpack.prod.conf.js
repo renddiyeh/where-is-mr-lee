8b4b628ef06bf97dfaffb1da0ff7b24376544766
Renddi <renddi@relab.cc>
2017-03-23 22:48:50 +0800

    configured shareholic and disqus

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -85,6 +85,10 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../CNAME'),
+        to: config.build.assetsRoot
       }
     ])
   ]
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
-    <div class="bg" :style="bgStyle">
-      <div class="main">
-        <div class="title">
-          <h1>{{ title }}</h1>
-          <h3>關注中國社會發展及維權人士的台灣非政府組織工作者李明哲，在2017年3月19日上午從澳門進入中國時失去聯絡...</h3>
-        </div>
-        <hr>
-        <div class="news-wrapper">
-          <h2>相關新聞：</h2>
-          <ul class="news">
-            <li v-for="item in news">
-              <a :href="item.url" target="_blank">{{ item.title }}</a>
-            </li>
-          </ul>
-        </div>
-        <div class="action">
-          <a class="share" :href="fbLink" target="_blank">希望你可以幫忙轉發這消息，運用網路的力量，協助找到李明哲</a>
-        </div>
+    <div class="bg" :style="bgStyle"></div>
+    <div class="main">
+      <div class="title">
+        <h1>{{ title }}</h1>
+        <h3>關注中國社會發展及維權人士的台灣非政府組織工作者李明哲，在2017年3月19日上午從澳門進入中國時失去聯絡...</h3>
       </div>
+      <hr>
+      <div class="news-wrapper">
+        <h2>相關新聞：</h2>
+        <ul class="news">
+          <li v-for="item in news">
+            <a :href="item.url" target="_blank">{{ item.title }}</a>
+          </li>
+        </ul>
+      </div>
+      <div class="action">
+        <p class="share">希望你可以幫忙轉發這消息，運用網路的力量，協助找到李明哲</p>
+        <div class="shareaholic-canvas" data-app="share_buttons" data-app-id="26706199"></div>
+      </div>
+      <div id="disqus_thread"></div>
     </div>
   </div>
 </template>
@@ -26,12 +27,20 @@
 <script>
 import BG from './assets/Guangzhou.jpg';
 
+(() => {
+  const d = document;
+  const s = d.createElement('script');
+  s.src = 'https://where-is-lee.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
+
 const title = '有人看見李明哲嗎？';
 const fbID = '1330773760349289';
 const fbParams = {
   app_id: fbID,
   display: 'popup',
-  href: '',
+  href: 'https://whereislee.org',
 };
 
 const fbParamsString = Object.keys(fbParams).map(key => `${key}=${fbParams[key]}&`);
@@ -77,7 +86,6 @@ html, body {
   text-align: center;
   width: 100%;
   height: 100%;
-
 }
 
 .bg::before {
@@ -87,8 +95,8 @@ html, body {
   bottom: 0;
   left: 0;
   right: 0;
-  box-shadow: inset 0 0 10em #333;
-  background-color: rgba(0, 0, 0, 0.3);
+  box-shadow: inset 0 0 10em rgba(0, 0, 0, 0.6);
+  background-color: rgba(0, 0, 0, 0.6);
   position: absolute;
   z-index: 1;
 }
@@ -98,7 +106,7 @@ html, body {
   height: 100%;
   background-size: cover;
   background-position: 50% 50%;
-  position: relative;
+  position: fixed;
   color: white;
   padding: 1em;
 }
@@ -111,10 +119,15 @@ a {
   top: 25%;
   z-index: 10;
   position: relative;
+  color: white;
+  max-width: 50em;
+  padding: 0 1em;
+  margin: 0 auto;
 }
 
 .title {
   margin-bottom: 2em;
+  line-height: 1.8;
 }
 
 .main h1 {
@@ -131,20 +144,15 @@ ul.news li {
 }
 
 .action {
-  padding-top: 2em;
-}
-
-.share {
   border: white 2px solid;
   padding: 2em;
-  text-decoration: none;
-  transition: background-color 500ms ease-in-out,
-    color 500ms ease-in-out;
+  display: inline-block;
 }
 
-.share:hover {
-  background-color: white;
-  color: black;
+#disqus_thread {
+  margin: 2em auto;
+  padding: 2em;
+  background-color: rgba(255, 255, 255, 0.7);
 }
 
 </style>
